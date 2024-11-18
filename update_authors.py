78475532c80669d27c7ca20b98d886a09f78e345
RizwanMunawar <chr043416@gmail.com>
2024-11-19 02:28:41 +0500

    update

--- update_authors.py
+++ update_authors.py
@@ -5,25 +5,42 @@ MKDOCS_YML = "mkdocs.yml"
 AUTHORS_JSON = "docs/authors.json"
 
 
-def inject_authors_into_mkdocs():
+def inject_plugin_into_mkdocs():
     # Load authors.json
     with open(AUTHORS_JSON, "r") as f:
         authors_data = json.load(f)
+
+    # Define the plugin configuration to be added
+    plugin_config = {
+        "git-authors": {
+            "github_usernames": authors_data
+        }
+    }
 
     # Load mkdocs.yml
     with open(MKDOCS_YML, "r") as f:
         mkdocs_config = yaml.safe_load(f)
 
-    # Inject authors data
-    mkdocs_config["extra"] = mkdocs_config.get("extra", {})
-    mkdocs_config["extra"]["authors"] = authors_data
+    # Ensure the plugins section exists
+    mkdocs_config["plugins"] = mkdocs_config.get("plugins", [])
+
+    # Check if 'git-authors' plugin is already in the plugins list
+    plugin_found = False
+    for plugin in mkdocs_config["plugins"]:
+        if isinstance(plugin, dict) and "git-authors" in plugin:
+            plugin["git-authors"] = plugin_config["git-authors"]
+            plugin_found = True
+            break
+
+    # Add 'git-authors' plugin if not already present
+    if not plugin_found:
+        mkdocs_config["plugins"].append(plugin_config)
 
     # Write back to mkdocs.yml
     with open(MKDOCS_YML, "w") as f:
-        yaml.dump(mkdocs_config, f)
+        yaml.dump(mkdocs_config, f, default_flow_style=False)
 
-    print("Injected authors into mkdocs.yml")
-
+    print("Injected git-authors plugin into mkdocs.yml")
 
 if __name__ == "__main__":
-    inject_authors_into_mkdocs()
+    inject_plugin_into_mkdocs()
